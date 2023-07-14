c49096dbad870d11ae8366e25e93742dcc266a56
kroos <dhiauddin@gmail.com>
2023-07-14 08:42:53 +0800

    -- remove gijgo adding pc bootstrap4 datetimepicker

--- resources/js/app.js
+++ resources/js/app.js
@@ -24,7 +24,8 @@ try {
     require('datatables.net-fixedheader-bs5');
     require('datatables.net-responsive-bs5');
     require('fullcalendar');
-    require('gijgo');
+    // require('gijgo');
+    require('pc-bootstrap4-datetimepicker');
     require('../../node_modules/jquery-chained/jquery.chained');
     require('../../node_modules/jquery-chained/jquery.chained.remote');
     require('../../node_modules/jquery-ui/dist/jquery-ui');
