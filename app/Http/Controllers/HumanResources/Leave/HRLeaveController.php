5c214afc279e06ea8f400b504a776786d5dfb6de
kroos <dhiauddin@gmail.com>
2023-08-04 01:59:21 +0800

    -- done with AL leave application

--- app/Http/Controllers/HumanResources/Leave/HRLeaveController.php
+++ app/Http/Controllers/HumanResources/Leave/HRLeaveController.php
@@ -58,58 +58,56 @@ class HRLeaveController extends Controller
 		// return $request->all();
 		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		// initial setup for create a leave
-		$user = \Auth::user()->belongstostaff;								// for specific user
-		$daStart = Carbon::parse($request->date_time_start);				// date start : for manipulation
+		$user = \Auth::user()->belongstostaff;																										// for specific user
+		$daStart = Carbon::parse($request->date_time_start);																						// date start : for manipulation
 
-
-		// in time off, there only date_time_start so...
-		if( empty( $request->date_time_end ) ) {
+		if( empty( $request->date_time_end ) ) {																									// in time off, there only date_time_start so...
 			$request->date_time_end = $request->date_time_start;
 		}
 
-		// count rows for particular year based on $request->date_time_start
-		$row = HRLeave::whereYear('date_time_start', $request->date_time_start)->get()->count();
-		$ye = $daStart->format('y');						// strip down to 2 digits
+		$row = HRLeave::whereYear('date_time_start', $request->date_time_start)->get()->count();													// count rows for particular year based on $request->date_time_start
+		$row += 1;
+		$ye = $daStart->format('y');																												// strip down to 2 digits
 
 		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		// if a user select more than 1 day and setting double date is on, we need to count the remaining day that is not overlapping
-			$blockdate = UnavailableDate::blockDate(\Auth::user()->belongstostaff->id);
-			$period = \Carbon\CarbonPeriod::create($request->date_time_start, '1 days', $request->date_time_end);
-			$lea = [];
-			foreach ($period as $value) {
-				$lea[] = Carbon::parse($value)->format('Y-m-d');
-			}
-			$totalday = count($lea);
+		$blockdate = UnavailableDate::blockDate(\Auth::user()->belongstostaff->id);
 
-			$leav = [];
-			foreach ($blockdate as $val1) {
-				$va1 = Carbon::parse($val1);
-				foreach ($period as $val2) {
-					if(Carbon::parse($val1)->equalTo(Carbon::parse($val2))){
-						$leav[] = Carbon::parse($val1)->format('Y-m-d');
-					}
+		$period = \Carbon\CarbonPeriod::create($request->date_time_start, '1 days', $request->date_time_end);
+		$lea = [];
+		foreach ($period as $value) {
+			$lea[] = Carbon::parse($value)->format('Y-m-d');
+		}
+		$totalday = count($lea);
+
+		$leav = [];
+		foreach ($blockdate as $val1) {
+			$va1 = Carbon::parse($val1);
+			foreach ($period as $val2) {
+				if(Carbon::parse($val1)->EqualTo(Carbon::parse($val2))){
+					$leav[] = Carbon::parse($val1)->format('Y-m-d');
 				}
 			}
-			$filtered = array_diff($lea, $leav);			// get all the dates that is not in $blockdate
-			$totaldayfiltered = count($filtered);			// total days
+		}
+		$filtered = array_diff($lea, $leav);																										// get all the dates that is not overlapped
+		$totaldayfiltered = count($filtered);																										// total days
 
+		// return $filtered;
 		// return $totaldayfiltered;
 		// exit;
 
-		$date = [];
+		$dateStartEnd = [];
 		if($totalday == $totaldayfiltered){
-			$noOverlap = true;							// meaning we CAN take $request->date_time_end $request->date_time_start as is to be insert in database
-			$dateStart = $request->date_time_start;
-			$dateEnd  = $request->date_time_end;
+			$noOverlap = true;																														// meaning we CAN take $request->date_time_end $request->date_time_start as is to be insert in database
 		} else {
-			$noOverlap = false;							// meaning we CANT take $request->date_time_end $request->date_time_start as is to be insert in database, instead we need to separate it row by row to be inserted into database.
+			$noOverlap = false;																														// meaning we CANT take $request->date_time_end $request->date_time_start as is to be insert in database, instead we need to separate it row by row to be inserted into database.
 			// we need to loop entire date which is available 1 by 1
 			foreach($filtered as $d){
-				$date[] = ['date_time_start' => $d, 'date_time_end' => $d];
+				$dateStartEnd[] = ['date_time_start' => $d, 'date_time_end' => $d];
 			}
 		}
-		$c = count($date);
-		// return $c;
+		// return $dateStartEnd;
+		// exit;
 
 		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		// generate code for approver
@@ -127,45 +125,147 @@ class HRLeaveController extends Controller
 		if($request->leave_type_id == 1 || $request->leave_type_id == 5) {
 			// check entitlement if configured or not
 			$entitlement = $user->hasmanyleaveentitlement()->where('year', $daStart->year)->first();
-			// if(!$entitlement) {								// kick him out if there is no entitlement been configured for entitlement
+			// if(!$entitlement) {																													// kick him out if there is no entitlement been configured for entitlement
 			// 	Session::flash('flash_message', 'Please contact with your Human Resources Manager. Most probably, HR havent configured yet your entitlement.');
 			// 	return redirect()->back();
 			// }
 
-			// if ($entitlement->al_balance >= $totaldayfiltered) {			// check also his entitlement
-				if ($request->has('leave_type')) {							// applied for 1 full day OR half day
-					if($request->leave_type == 2){							// half day
+			if ($request->has('leave_type')) {																										// applied for 1 full day OR half day
+				if($request->leave_type == 2){																										// half day
+					if($entitlement->al_balance >= 0.5){																							// al_balance > 0.5
+						$entitle = $entitlement->al_balance - 0.5;
+
 						$data = $request->only(['leave_type_id', 'reason', 'date_time_start', 'date_time_end', 'half_type_id']);
 						$data += ['verify_code' => $code];
 						$data += ['period_day' => 0.5];
-						$data += ['leave_no' => ($row + 1)];
+						$data += ['leave_no' => $row];
 						$data += ['leave_year' => $ye];
 
-						$l = $user->hasmanyleave()->create($data);						// insert data into HRLeave
-						$user->hasmanyleaveentitlement()->where('year', $daStart->year)->update(['al_balance' => ($entitlement->al_balance - 0.5)]);						// substract al_balance
-					} elseif($request->leave_type == 1) {												// full 1 day
+						$l = $user->hasmanyleave()->create($data);																					// insert data into HRLeave
+						$user->hasmanyleaveentitlement()->where('year', $daStart->year)->update(['al_balance' => $entitle]);						// update al_balance by substarct
+						if($user->belongstoleaveapprovalflow->backup_approval == 1){																// alert backup
+							$l->hasoneleaveapprovalbackup()->create($request->only(['staff_id']));
+						}
+						if($user->belongstoleaveapprovalflow->supervisor_approval == 1){															// alert supervisor
+							$l->hasoneleaveapprovalsupervisor()->create();
+						}
+						if($user->belongstoleaveapprovalflow->hod_approval == 1){																	// alert hod
+							$l->hasoneleaveapprovalhod()->create();
+						}
+						if($user->belongstoleaveapprovalflow->director_approval == 1){																// alert director
+							$l->hasoneleaveapprovaldir()->create();
+						}
+						if($user->belongstoleaveapprovalflow->hr_approval == 1){																	// alert hr
+							$l->hasoneleaveapprovalhr()->create();
+						}
+					} else {
+						Session::flash('flash_message', 'Please check your entitlement based on the date leave you apply');
+						return redirect()->back();
+					}
+				} elseif($request->leave_type == 1) {																								// apply leace 1 whole day
+					if($entitlement->al_balance >= 1){																								// al_balance >= 1
+						$entitle = $entitlement->al_balance - 1;
+
 						$data = $request->only(['leave_type_id', 'reason', 'date_time_start', 'date_time_end', 'half_type_id']);
 						$data += ['verify_code' => $code];
 						$data += ['period_day' => 1];
-						$data += ['leave_no' => ($row + 1)];
+						$data += ['leave_no' => $row];
 						$data += ['leave_year' => $ye];
 
-						$l = $user->hasmanyleave()->create($data);						// insert data into HRLeave
-						$user->hasmanyleaveentitlement()->where('year', $daStart->year)->update(['al_balance' => ($entitlement->al_balance - 1)]);		// substract al_balance
+						$l = $user->hasmanyleave()->create($data);																					// insert data into HRLeave
+						$user->hasmanyleaveentitlement()->where('year', $daStart->year)->update(['al_balance' => $entitle]);						// substract al_balance
+						if($user->belongstoleaveapprovalflow->backup_approval == 1){																// alert backup
+							$l->hasoneleaveapprovalbackup()->create($request->only(['staff_id']));
+						}
+						if($user->belongstoleaveapprovalflow->supervisor_approval == 1){															// alert supervisor
+							$l->hasoneleaveapprovalsupervisor()->create();
+						}
+						if($user->belongstoleaveapprovalflow->hod_approval == 1){																	// alert hod
+							$l->hasoneleaveapprovalhod()->create();
+						}
+						if($user->belongstoleaveapprovalflow->director_approval == 1){																// alert director
+							$l->hasoneleaveapprovaldir()->create();
+						}
+						if($user->belongstoleaveapprovalflow->hr_approval == 1){																	// alert hr
+							$l->hasoneleaveapprovalhr()->create();
+						}
+					} else {
+						Session::flash('flash_message', 'Please check your entitlement based on the date leave you apply');
+						return redirect()->back();
 					}
-				} else {											// apply leave for 2 OR more days
+				}
+			} else {																																// apply leave for 2 OR more days
+				if ($noOverlap) {																													// true: date choose not overlapping date with unavailable date
+					if($entitlement->al_balance >= $totalday) {																						// al_balance > $totalday
+						$entitle = $entitlement->al_balance - $totalday;
 
+						$data = $request->only(['leave_type_id', 'reason', 'date_time_start', 'date_time_end']);
+						$data += ['verify_code' => $code];
+						$data += ['period_day' => $totalday];
+						$data += ['leave_no' => $row];
+						$data += ['leave_year' => $ye];
+
+						$l = $user->hasmanyleave()->create($data);																					// insert data into HRLeave
+						$user->hasmanyleaveentitlement()->where('year', $daStart->year)->update(['al_balance' => $entitle]);						// substract al_balance
+						if($user->belongstoleaveapprovalflow->backup_approval == 1){																// alert backup
+							$l->hasoneleaveapprovalbackup()->create($request->only(['staff_id']));
+						}
+						if($user->belongstoleaveapprovalflow->supervisor_approval == 1){															// alert supervisor
+							$l->hasoneleaveapprovalsupervisor()->create();
+						}
+						if($user->belongstoleaveapprovalflow->hod_approval == 1){																	// alert hod
+							$l->hasoneleaveapprovalhod()->create();
+						}
+						if($user->belongstoleaveapprovalflow->director_approval == 1){																// alert director
+							$l->hasoneleaveapprovaldir()->create();
+						}
+						if($user->belongstoleaveapprovalflow->hr_approval == 1){																	// alert hr
+							$l->hasoneleaveapprovalhr()->create();
+						}
+					} else {																														// al_balance < $totalday, then exit
+						Session::flash('flash_message', 'Please check your entitlement based on the date leave you apply');
+						return redirect()->back();
+					}
+				} else {
+					// since date_time_start and date_time_end overlapping with block date, need to iterate date by date																															// false: date choose overlapping date with unavailable date
+					if($entitlement->al_balance >= $totaldayfiltered) {																				// al_balance > $totaldayfiltered
+						$entitle = $entitlement->al_balance - $totalday;
+
+						foreach ($dateStartEnd as $value) {
+							$data = [
+								'date_time_start' => $value['date_time_start'],
+								'date_time_end' => $value['date_time_end'],
+								'verify_code' => $code,
+								'period_day' => 1,
+								'leave_no' => $row++,
+								'leave_year' => $ye,
+								'leave_type_id' => $request->leave_type_id,
+								'reason' => $request->reason
+							];
+							$l = $user->hasmanyleave()->create($data);																				// insert data into HRLeave
+							if($user->belongstoleaveapprovalflow->backup_approval == 1){															// alert backup
+								$l->hasoneleaveapprovalbackup()->create($request->only(['staff_id']));
+							}
+							if($user->belongstoleaveapprovalflow->supervisor_approval == 1){														// alert supervisor
+								$l->hasoneleaveapprovalsupervisor()->create();
+							}
+							if($user->belongstoleaveapprovalflow->hod_approval == 1){																// alert hod
+								$l->hasoneleaveapprovalhod()->create();
+							}
+							if($user->belongstoleaveapprovalflow->director_approval == 1){															// alert director
+								$l->hasoneleaveapprovaldir()->create();
+							}
+							if($user->belongstoleaveapprovalflow->hr_approval == 1){																// alert hr
+								$l->hasoneleaveapprovalhr()->create();
+							}
+						}
+						$user->hasmanyleaveentitlement()->where('year', $daStart->year)->update(['al_balance' => $entitle]);						// substract al_balance
+					} else {																														// al_balance < $totalday, then exit
+						Session::flash('flash_message', 'Please check your entitlement based on the date leave you apply');
+						return redirect()->back();
+					}
 				}
-			// } else {
-			// 	Session::flash('flash_message', 'Please check your entitlement based on the date leave you applied');
-			// 	return redirect()->back();
-			// }
-				// check date as above
-				if ($noOverlap) {				// true: date choose not overlapping date with unavailable date
-					// $l = $user->hasmanyleave()->insert($request->only(Arr::add(['leave_type_id', 'reason', 'date_time_start', 'date_time_end'], 'verify_code', $code)));
-				} else {						// false: date choose overlapping date with unavailable date
-					
-				}
+			}
 		}
 
 		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -177,14 +277,12 @@ class HRLeaveController extends Controller
 		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		// MC
 		if($request->leave_type_id == 2) {
-			// solve date part
 
 		}
 
 		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		// NRL & EL-NRL
 		if($request->leave_type_id == 4 || $request->leave_type_id == 10) {
-			// solve date part
 
 		}
 
@@ -203,69 +301,12 @@ class HRLeaveController extends Controller
 		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		// S-UPL
 		if($request->leave_type_id == 12) {
-			// solve date part
 
 		}
-
-		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		// insert leave id into these table apporover (supervisor, HOD, director, HR)
-		if($user->belongstoleaveapprovalflow->backup_approval == 1){
-			$l->hasoneleaveapprovalbackup()->create($request->only(['staff_id']));
-		}
-
-		//////////////////////////////////////////////////////////////////////////////
-		// this section need to be look once there is more branch and category
-		if($user->belongstoleaveapprovalflow->supervisor_approval == 1){				//supervisor: div_id = 4
-			$l->hasoneleaveapprovalsupervisor()->create();
-		}
-
-		//////////////////////////////////////////////////////////////////////////////
-		if($user->belongstoleaveapprovalflow->hod_approval == 1){
-			// search hod div_id = 4
-			$l->hasoneleaveapprovalhod()->create();
-		}
-
-		//////////////////////////////////////////////////////////////////////////////
-		if($user->belongstoleaveapprovalflow->director_approval == 1){
-			$l->hasoneleaveapprovaldir()->create();
-		}
-
-		//////////////////////////////////////////////////////////////////////////////
-		if($user->belongstoleaveapprovalflow->hr_approval == 1){
-			$l->hasoneleaveapprovalhr()->create();
-		}
-		//end section
 		//////////////////////////////////////////////////////////////////////////////
 		Session::flash('flash_message', 'Successfully Applied Leave.');
-		return redirect()->back();
+		return redirect()->route('leave.index');
 		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 	}
 
 	/**
