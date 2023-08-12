6dc413215c3f3b51adda0b42f6c54e1afd5fc975
kroos <dhiauddin@gmail.com>
2023-08-12 14:42:34 +0800

    working on staff

--- resources/views/humanresources/hrdept/staff/index.blade.php
+++ resources/views/humanresources/hrdept/staff/index.blade.php
@@ -5,7 +5,7 @@
 use App\Models\Staff;
 
 ?>
-<div class="col-sm-12 table-responsive">
+<div class="col table-responsive">
 	@include('humanresources.hrdept.navhr')
 	<p>Staffs</p>
 	<table id="staff" class="table table-hover table-sm align-middle" style="font-size:12px">
@@ -25,13 +25,13 @@ use App\Models\Staff;
 				<th>Location</th>
 				<th>Leave Flow</th>
 				<th>Phone</th>
-				<th>CIMB Acc</th>
+<!-- 				<th>CIMB Acc</th>
 				<th>EPF</th>
 				<th>Income Tax</th>
 				<th>SOCSO</th>
 				<th>Join</th>
 				<th>Confirmed</th>
-			</tr>
+ -->			</tr>
 		</thead>
 		<tbody class="table-group-divider">
 			@foreach(Staff::where('active', 1)->get() as $s)
@@ -39,7 +39,7 @@ use App\Models\Staff;
 				@if(auth()->user()->belongstostaff->authorise_id == 1)
 				<td>{{ $s->id }}</td>
 				@endif
-				<td>{{ $s->hasmanylogin()->where('active', 1)->first()->username }}</td>
+				<td><a href="{{ route('staff.show', $s->id) }}" alt="Detail" title="Detail">{{ $s->hasmanylogin()->where('active', 1)->first()->username }}</a></td>
 				<td data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" data-bs-html="true" data-bs-title="
 					<div class='d-flex flex-column align-items-center text-center p-3 py-5'>
 						<img class='rounded-5 mt-3' width='180px' src='{{ asset('storage/user_profile/' . $s->image) }}'>
@@ -57,12 +57,12 @@ use App\Models\Staff;
 				<td>{{ $s->belongstomanydepartment()->wherePivot('main', 1)->first()?->belongstobranch->location }}</td>
 				<td>{{ $s->belongstoleaveapprovalflow?->description }}</td>
 				<td>{{ $s->mobile }}</td>
-				<td>{{ $s->cimb_account }}</td>
+<!-- 				<td>{{ $s->cimb_account }}</td>
 				<td>{{ $s->epf_account }}</td>
 				<td>{{ $s->income_tax_no }}</td>
 				<td>{{ $s->socso_no }}</td>
 				<td>{{ \Carbon\Carbon::parse($s->join)->format('j M Y ') }}</td>
-				<td>{{ \Carbon\Carbon::parse($s->confirmed)->format('j M Y ') }}</td>
+				<td>{{ \Carbon\Carbon::parse($s->confirmed)->format('j M Y ') }}</td> -->
 			</tr>
 			@endforeach
 		</tbody>
@@ -86,10 +86,11 @@ $('#staff').DataTable({
 	"order": [[1, "asc" ]],	// sorting the 6th column descending
 	responsive: true
 })
-.on( 'length.dt', function ( e, settings, len ) {
-$(document).ready(function(){
-	$('[data-bs-toggle="tooltip"]').tooltip();
-});} );
+.on( 'length.dt page.dt order.dt search.dt', function ( e, settings, len ) {
+	$(document).ready(function(){
+		$('[data-bs-toggle="tooltip"]').tooltip();
+	});}
+ );
 
 
 @endsection
