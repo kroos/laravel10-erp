bdd645cc1135fcb1b0e7cfc2f4a721b03027013f
ipma <it.ipmaindustry@gmail.com>
2023-09-22 18:00:41 +0800

    -- replacement -- resources/views/humanresources/hrdept/rleave/index.blade.php

--- resources/views/humanresources/hrdept/rleave/index.blade.php
+++ resources/views/humanresources/hrdept/rleave/index.blade.php
@@ -6,6 +6,10 @@
   /* div {
   border: 1px solid black;
 } */
+
+table, thead, tr, th {
+  border: 1px solid black;
+}
 </style>
 
 <div class="container">
@@ -32,22 +36,22 @@
       <tbody>
         @foreach ($replacements as $replacement)
         <tr>
-          <th>ID</th>
-          <th>{{ $replacement->belongstostaff->name }}</th>
+          <th>{{ $replacement->belongstostaff->hasmanylogin()->where('logins.active', 1)->first()->username }}</th>
+          <th class="text-truncate" style="max-width: 200px;">{{ $replacement->belongstostaff->name }}</th>
           <th>{{ $replacement->date_start }}</th>
           <th>{{ $replacement->date_end }}</th>
-          <th>
+          <th class="text-truncate" style="max-width: 200px;">
             @if ($replacement->belongstocustomer)
               {{ $replacement->belongstocustomer->customer }}
             @endif
           </th>
-          <th>{{ $replacement->reason }}</th>
-          <th>Total</th>
-          <th>Utilize</th>
-          <th>Balance</th>
-          <th>Remarks</th>
-          <th>Edit</th>
-          <th>Cancel</th>
+          <th class="text-truncate" style="max-width: 150px;">{{ $replacement->reason }}</th>
+          <th class="text-center">{{ $replacement->leave_total }}</th>
+          <th class="text-center">{{ $replacement->leave_utilize }}</th>
+          <th class="text-center">{{ $replacement->leave_balance }}</th>
+          <th class="text-truncate" style="max-width: 100px;">{{ $replacement->remarks }}</th>
+          <th><a href="#" class="btn btn-outline-secondary" role="button" aria-pressed="true" style="--bs-btn-padding-y: 0px; --bs-btn-padding-x: 0px; --bs-btn-font-size: .75rem;">EDIT</a></th>
+          <th><a href="#" class="btn btn-outline-secondary" role="button" aria-pressed="true" style="--bs-btn-padding-y: 0px; --bs-btn-padding-x: 0px; --bs-btn-font-size: .75rem;">CANCEL</a></th>
         </tr>
         @endforeach
       </tbody>
@@ -80,7 +84,25 @@
 
 
 @section('js')
-
+/////////////////////////////////////////////////////////////////////////////////////////
+// datatables
+$.fn.dataTable.moment( 'D MMM YYYY' );
+$.fn.dataTable.moment( 'h:mm a' );
+$('#replacement').DataTable({
+"paging": false,
+"lengthMenu": [ [-1], ["All"] ],
+"columnDefs": [
+{ type: 'date', 'targets': [2] },
+{ type: 'time', 'targets': [3] },
+],
+"order": [ 0, 'asc' ], // sorting the 6th column descending
+responsive: true
+})
+.on( 'length.dt page.dt order.dt search.dt', function ( e, settings, len ) {
+$(document).ready(function(){
+$('[data-bs-toggle="tooltip"]').tooltip();
+});}
+);
 @endsection
 
 @section('nonjquery')
