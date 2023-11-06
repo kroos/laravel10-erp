2624d8b9afbaed69d8c14421de22a8b50294cbf5
kroos <dhiauddin@gmail.com>
2023-11-06 14:19:51 +0800

    embed youtube

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,37 @@
 
 @section('content')
 
+
+
+<div class="container-fluid d-flex flex-column align-items-center text-center p-3 py-5">
+	<h1>How To Apply Leave</h1>
+	<iframe width="560" height="315" src="https://www.youtube.com/embed/j6PtxOLRo48?si=6Q3GVP7yHmHY6I48" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @endsection
 
 @section('js')
