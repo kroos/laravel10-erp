04b71f7676924e319aba9051337a329bd82badc6
kroos <dhiauddin@gmail.com>
2023-11-08 18:02:31 +0800

    -- working on excel generate

--- app/Exports/PayslipExport.php
+++ app/Exports/PayslipExport.php
@@ -34,18 +34,53 @@ class PayslipExport implements FromCollection
 		// return HRAttendance::all();
 		$req = $this->request;
 		// dd($req['from']);
-		$staff = HRAttendance::select()
-				->where(function (Builder $query) use ($req){
-									$query->whereDate('attend_date', '>=', $req['from'])
-										->whereDate('attend_date', '<=', $req['to']);
+		$hratt = HRAttendance::where(function (Builder $query) use ($req){
+					$query->whereDate('attend_date', '>=', $req['from'])
+						->whereDate('attend_date', '<=', $req['to']);
 				})
 				->groupBy('staff_id')
 				// ->ddrawsql();
 				->get();
 
 		$header = ['Emp No', 'Name', 'AL', 'NRL', 'MC', 'UPL', 'Absent', 'UPMC', 'Lateness', 'Early Out', 'No Pay Hour', 'Maternity', 'Hospitalization', 'Other Leave', 'Compassionate Leave', 'Marriage Leave', 'Day Work', '1.0 OT', '1.5 OT', 'OT'];
-		$records = ['Emp No', 'Name', 'AL', 'NRL', 'MC', 'UPL', 'Absent', 'UPMC', 'Lateness', 'Early Out', 'No Pay Hour', 'Maternity', 'Hospitalization', 'Other Leave', 'Compassionate Leave', 'Marriage Leave', 'Day Work', '1.0 OT', '1.5 OT', 'OT'];
+		$records = [];
 
+		foreach ($hratt as $k1 => $v1) {
+			$login = Login::where('staff_id', $v1->staff_id)->first()?->username;
+			$name = Staff::find($v1->staff_id)->name;
+			$al = HRAttendance::where(function (Builder $query) use ($req){
+					$query->whereDate('attend_date', '>=', $req['from'])
+						->whereDate('attend_date', '<=', $req['to']);
+				})
+				->
+				->whereNotNull('leave_id')
+				->get();
+			if($al->count()) {
+
+			}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			$records[$k1] = [$login, $name];
+		}
+		dd($records);
 
 
 
