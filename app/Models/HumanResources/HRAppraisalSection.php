4a2fd6c07be2a98f6ce15a07f4e3cbcd93a4c411
ipma <it.ipmaindustry@gmail.com>
2023-12-29 18:01:40 +0800

    -- appraisal -- app/Http/Controllers/HumanResources/HRDept/AppraisalFormController.php -- app/Models/HumanResources/DepartmentPivot.php -- app/Models/HumanResources/HRAppraisalSection.php -- resources/views/humanresources/hrdept/appraisal/form/create.blade.php -- resources/views/humanresources/hrdept/appraisal/form/index.blade.php

--- app/Models/HumanResources/HRAppraisalSection.php
+++ app/Models/HumanResources/HRAppraisalSection.php
@@ -13,11 +13,17 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 // use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-// use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class HRAppraisalSection extends Model
 {
     use HasFactory;
     // protected $connection = 'mysql';
     protected $table = 'hr_appraisal_sections';
+
+    ////////////////////////// belongsToMany //////////////////////////
+    public function belongstomanydepartmentpivot(): BelongsToMany
+	{
+		return $this->belongsToMany(\App\Models\HumanResources\DepartmentPivot::class, 'pivot_dept_appraisals', 'section_id', 'department_id')->withTimestamps();
+	}
 }
--- resources/views/humanresources/hrdept/appraisal/form/index.blade.php
+++ resources/views/humanresources/hrdept/appraisal/form/index.blade.php
@@ -3,31 +3,36 @@
 @section('content')
 
 <style>
-	/* div {
-		border: 1px solid red;
-	} */
+  div {
+    border: 1px solid red;
+  }
 </style>
 
 
 <div class="container">
-	@include('humanresources.hrdept.navhr')
+  @include('humanresources.hrdept.navhr')
 
-	<h4>Appraisal Form</h4>
+  <h4>Appraisal Form</h4>
 
-	<div class="row">&nbsp;</div>
+  <div class="row">&nbsp;</div>
 
-	@foreach ($departments as $department)
-	<div class="row" style="background-color: #f0f0f0; font-size: 20px;">
-		<div class="col-sm-12">
-			<a class="btn btn-primary btn-sm" href="{{ route('appraisalform.create', ['id' => $department->id]) }}" role="button" >+</a> 
-			{{ $department->department }}
-		</div>
-	</div>
-	<div class="row">
+  @foreach ($departments as $department)
+  <div class="row" style="background-color: #f0f0f0; font-size: 20px;">
+    <div class="col-sm-12">
+      <a class="btn btn-primary btn-sm" href="{{ route('appraisalform.create', ['id' => $department->id]) }}" role="button">+</a>
+      {{ $department->department }}
+    </div>
+  </div>
+  <div class="row">
+    <div align="right" style="width: 80px;">
+      <i class="bi bi-caret-right-fill"></i>
+    </div>
+    <div>
 
-	</div>
-	<div class="row">&nbsp;</div>
-	@endforeach
+    </div>
+  </div>
+  <div class="row">&nbsp;</div>
+  @endforeach
 
 </div>
 @endsection
