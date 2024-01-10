875b36557faba636a9fa9864098e09281cc0d047
kroos <dhiauddin@gmail.com>
2024-01-10 10:36:15 +0800

    -- create folder attendance in storage

--- resources/views/humanresources/hrdept/attendance/attendanceupload/create.blade.php
+++ resources/views/humanresources/hrdept/attendance/attendanceupload/create.blade.php
@@ -2,39 +2,27 @@
 
 @section('content')
 <div class="container">
-  @include('humanresources.hrdept.navhr')
+	@include('humanresources.hrdept.navhr')
+	<h4>Attendance Upload</h4>
 
-  <div class="row mt-3"></div>
+	{{ Form::open(['route' => ['attendanceupload.store'], 'id' => 'form', 'class' => 'form-horizontal', 'autocomplete' => 'off', 'files' => true]) }}
 
-  <h4>Attendance Upload</h4>
+	<div class="row mt-3">
+		<div class="col-md-2">
+			{{Form::label('softcopy', 'Excel File')}}
+		</div>
+		<div class="col-md-10">
+			{!! Form::file('softcopy', ['class' => 'form-control', 'id' => 'softcopy']) !!}
+		</div>
+	</div>
 
-  {{ Form::open(['route' => ['attendanceupload.store'], 'id' => 'form', 'class' => 'form-horizontal', 'autocomplete' => 'off', 'files' => true]) }}
+	<div class="row mt-3">
+		<div class="col-md-12 text-center">
+			{!! Form::submit('SUBMIT', ['class' => 'btn btn-sm btn-outline-secondary']) !!}
+		</div>
+	</div>
 
-  <div class="row mt-3">
-    <div class="col-md-2">
-      {{Form::label('softcopy', 'Excel File')}}
-    </div>
-    <div class="col-md-10">
-      {!! Form::file('softcopy', ['class' => 'form-control', 'id' => 'softcopy']) !!}
-    </div>
-  </div>
-
-  <div class="row mt-3">
-    <div class="col-md-12 text-center">
-      {!! Form::submit('SUBMIT', ['class' => 'btn btn-sm btn-outline-secondary']) !!}
-    </div>
-  </div>
-
-  {!! Form::close() !!}
-
-  <div class="row mt-3">
-    <div class="col-md-12 text-center">
-      <a href="{{ url()->previous() }}">
-        <button class="btn btn-sm btn-outline-secondary">BACK</button>
-      </a>
-    </div>
-  </div>
-
+	{!! Form::close() !!}
 </div>
 @endsection
 
@@ -42,25 +30,25 @@
 /////////////////////////////////////////////////////////////////////////////////////////
 // VALIDATOR
 $(document).ready(function() {
-$('#form').bootstrapValidator({
-feedbackIcons: {
-valid: '',
-invalid: '',
-validating: ''
-},
+	$('#form').bootstrapValidator({
+		feedbackIcons: {
+			valid: '',
+			invalid: '',
+			validating: ''
+		},
 
-fields: {
-softcopy: {
-validators: {
-file: {
-extension: 'xls,xlsx', // no space
-type: 'application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', // no space
-message: 'The selected file is not valid. Please use xls or xlsx file format.'
-},
-}
-},
+		fields: {
+			softcopy: {
+				validators: {
+					file: {
+						extension: 'xls,xlsx', // no space
+						type: 'application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', // no space
+						message: 'The selected file is not valid. Please use xls or xlsx file format.'
+					},
+				}
+			},
 
-}
-})
+		}
+	})
 });
 @endsection
