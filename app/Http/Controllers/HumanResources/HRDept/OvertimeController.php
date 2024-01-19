43c3452d232a516b917879b1c6da10b5a966fb7a
ipma <it.ipmaindustry@gmail.com>
2024-01-19 17:51:17 +0800

    -- controller update -- app/Http/Controllers/HumanResources/HRDept/AttendanceController.php -- app/Http/Controllers/HumanResources/HRDept/OvertimeController.php

--- app/Http/Controllers/HumanResources/HRDept/OvertimeController.php
+++ app/Http/Controllers/HumanResources/HRDept/OvertimeController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers\HumanResources\HRDept;
 
 use App\Http\Controllers\Controller;
@@ -29,113 +30,136 @@ use Session;
 
 class OvertimeController extends Controller
 {
-	function __construct()
-	{
-		$this->middleware(['auth']);
-		$this->middleware('highMgmtAccess:1|2|4|5,NULL', ['only' => ['create', 'store', 'index', 'show']]);			// all high management
-		$this->middleware('highMgmtAccessLevel1:1|5,14', ['only' => ['edit', 'update', 'destroy']]);						// only hod and asst hod HR can access
-	}
+  function __construct()
+  {
+    $this->middleware(['auth']);
+    $this->middleware('highMgmtAccess:1|2|4|5,NULL', ['only' => ['create', 'store', 'index', 'show']]);      // all high management
+    $this->middleware('highMgmtAccessLevel1:1|5,14', ['only' => ['edit', 'update', 'destroy']]);            // only hod and asst hod HR can access
+  }
 
-	/**
-	 * Display a listing of the resource.
-	 */
-	public function index(): View
-	{
-		Paginator::useBootstrapFive();
-		// $sa = HROvertime::SelectRaw('COUNT(staff_id) as totalstaff, ot_date')
-		// 				->where('active', 1)
-		// 				// ->groupByRaw('YEAR(ot_date)')
-		// 				->groupByRaw('MONTH(ot_date)')
-		// 				->orderBy('ot_date', 'DESC')
-		// 				// ->get();
-		// 				// ->ddRawSql();
-		// 				->cursorPaginate(1);
-		// // dd($sa);
-		// $overtime = HROvertime::select('*')
-		// 				// ->whereYear('ot_date', $sa->first()?->ot_date)
-		// 				->where('active', 1)
-		// 				->orderBy('ot_date', 'DESC')
-		// 				->cursorPaginate($sa->first()?->totalstaff);
+  /**
+   * Display a listing of the resource.
+   */
+  public function index(): View
+  {
+    Paginator::useBootstrapFive();
+    // $sa = HROvertime::SelectRaw('COUNT(staff_id) as totalstaff, ot_date')
+    // 				->where('active', 1)
+    // 				// ->groupByRaw('YEAR(ot_date)')
+    // 				->groupByRaw('MONTH(ot_date)')
+    // 				->orderBy('ot_date', 'DESC')
+    // 				// ->get();
+    // 				// ->ddRawSql();
+    // 				->cursorPaginate(1);
+    // // dd($sa);
+    // $overtime = HROvertime::select('*')
+    // 				// ->whereYear('ot_date', $sa->first()?->ot_date)
+    // 				->where('active', 1)
+    // 				->orderBy('ot_date', 'DESC')
+    // 				->cursorPaginate($sa->first()?->totalstaff);
 
-		$overtime = HROvertime::select('*')
-						// ->whereYear('ot_date', $sa->first()?->ot_date)
-						->where('active', 1)
-						->orderBy('ot_date', 'DESC')
-						->get();
+    $overtime = HROvertime::select('*')
+      // ->whereYear('ot_date', $sa->first()?->ot_date)
+      ->where('active', 1)
+      ->orderBy('ot_date', 'DESC')
+      ->get();
 
-		return view('humanresources.hrdept.overtime.index', ['overtime' => $overtime/*, 'sa' => $sa*/]);
-	}
+    return view('humanresources.hrdept.overtime.index', ['overtime' => $overtime/*, 'sa' => $sa*/]);
+  }
 
-	/**
-	 * Show the form for creating a new resource.
-	 */
-	public function create(): View
-	{
-		return view('humanresources.hrdept.overtime.create');
-	}
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create(): View
+  {
+    return view('humanresources.hrdept.overtime.create');
+  }
 
-	/**
-	 * Store a newly created resource in storage.
-	 */
-	public function store(Request $request): RedirectResponse
-	{
-		// dd($request->all());
-		foreach ($request->staff_id as $v) {
-			HROvertime::create([
-									'staff_id' => $v,
-									'ot_date' => $request->ot_date,
-									'overtime_range_id' => $request->overtime_range_id,
-									'active' => 1,
-									'assign_staff_id' => \Auth::user()->belongstostaff->id,
-									'remark' => ucwords(Str::of($request->remark)->lower()),
-								]);
-		}
-		Session::flash('flash_message', 'Successfully Add Staff Overtime');
-		return redirect()->route('overtime.index');
-	}
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request): RedirectResponse
+  {
+    // dd($request->all());
+    foreach ($request->staff_id as $v) {
 
-	/**
-	 * Display the specified resource.
-	 */
-	public function show(HROvertime $overtime): View
-	{
-		return view('humanresources.hrdept.overtime.show', ['overtime' => $overtime]);
-	}
+      if ($request->remark != NULL || $request->remark != "") {
+        $remark = ucwords(Str::of($request->remark)->lower());
+      } else {
+        $remark = NULL;
+      }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 */
-	public function edit(HROvertime $overtime): View
-	{
-		return view('humanresources.hrdept.overtime.edit', ['overtime' => $overtime]);
-	}
+      HROvertime::create([
+        'staff_id' => $v,
+        'ot_date' => $request->ot_date,
+        'overtime_range_id' => $request->overtime_range_id,
+        'active' => 1,
+        'assign_staff_id' => \Auth::user()->belongstostaff->id,
+        'remark' => $remark,
+      ]);
+    }
+    Session::flash('flash_message', 'Successfully Add Staff Overtime');
+    return redirect()->route('overtime.index');
+  }
 
-	/**
-	 * Update the specified resource in storage.
-	 */
-	public function update(Request $request, HROvertime $overtime): RedirectResponse
-	{
-		$dat = Arr::add($request->only(['ot_date', 'overtime_range_id', 'staff_id']), 'assign_staff_id', \Auth::user()->belongstostaff->id);
-		$data = Arr::add($dat, 'remarks', ucwords(Str::of($request->remark)->lower()));
-		$overtime->update();
-		Session::flash('flash_message', 'Successfully Update Staff Overtime');
-		return redirect()->route('overtime.index');
-	}
+  /**
+   * Display the specified resource.
+   */
+  public function show(HROvertime $overtime): View
+  {
+    return view('humanresources.hrdept.overtime.show', ['overtime' => $overtime]);
+  }
 
-	/**
-	 * Remove the specified resource from storage.
-	 */
-	public function destroy(HROvertime $overtime): JsonResponse
-	{
-		// remove from attendance
-		$r = HRAttendance::where('overtime_id', $overtime->id)->get();
-		foreach ($r as $c) {
-			HRAttendance::where('id', $c->id)->update(['overtime_id' => null]);
-		}
-		$overtime->update(['active' => NULL]);
-		return response()->json([
-			'message' => 'Data deleted',
-			'status' => 'success'
-		]);
-	}
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(HROvertime $overtime): View
+  {
+    return view('humanresources.hrdept.overtime.edit', ['overtime' => $overtime]);
+  }
+
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, HROvertime $overtime): RedirectResponse
+  {
+    if ($request->remark != NULL || $request->remark != "") {
+      $remark = ucwords(Str::of($request->remark)->lower());
+    } else {
+      $remark = NULL;
+    }
+
+    $overtime->update([
+      'staff_id' => $request->staff_id,
+      'ot_date' => $request->ot_date,
+      'overtime_range_id' => $request->overtime_range_id,
+      'assign_staff_id' => \Auth::user()->belongstostaff->id,
+      'remark' => $remark,
+    ]);
+
+    $overtime->save();
+
+    Session::flash('flash_message', 'Successfully Update Staff Overtime');
+    return redirect()->route('overtime.index');
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(HROvertime $overtime): JsonResponse
+  {
+    // remove from attendance
+    $r = HRAttendance::where('overtime_id', $overtime->id)->get();
+
+    foreach ($r as $c) {
+      HRAttendance::where('id', $c->id)->update(['overtime_id' => null]);
+    }
+
+    $overtime->update(['active' => NULL]);
+
+    return response()->json([
+      'message' => 'Overtime Deleted',
+      'status' => 'success'
+    ]);
+  }
 }
