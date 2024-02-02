fe063d91a786f43cb433f4b1ae807dd5ba859594
ipma <it.ipmaindustry@gmail.com>
2024-02-02 17:37:47 +0800

    -- sales -- resources/views/sales/salescustomer/index.blade.php -- resources/views/sales/salesdept/navhr.blade.php -- routes/Sales/sales.php -- app/Http/Controllers/Sales/SalesCustomerController.php

---
+++ resources/views/sales/salescustomer/index.blade.php
@@ -0,0 +1,94 @@
+@extends('layouts.app')
+
+@section('content')
+<style>
+div, table, tr, td {
+  border: 1px solid black;
+}
+
+</style>
+
+<?php
+$no = 1;
+?>
+
+<div class="container">
+  @include('sales.salesdept.navhr')
+
+  <div class="row mt-3">
+    <div class="col-md-2">
+      <h4>Customer</h4>
+    </div>
+    <div class="col-md-10">
+      <a href="{{ route('salescustomer.create') }}" class="btn btn-sm btn-outline-secondary">
+        Add Customer
+      </a>
+    </div>
+  </div>
+
+  <div class="mt-3">
+    <table id="customer" class="table table-hover table-sm align-middle" style="font-size:12px">
+      <thead>
+        <tr>
+          <th class="text-center" style="width: 20px;">ID</th>
+          <th class="text-center" style="width: 200px;">Customer</th>
+          <th class="text-center" style="width: 150px;">Contact</th>
+          <th class="text-center">Address</th>
+          <th class="text-center" style="width: 100px;">Phone No</th>
+
+
+          <th class="text-center" style="width: 40px;"></th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($customers as $customer)
+        <tr>
+          <td class="text-center">
+            {{ $no++ }}
+          </td>
+          <td>
+            <div>
+            {{ $customer->customer }}
+          </td>
+          <td class="text-center">
+            
+          </td>
+          <td class="text-center">
+            
+          </td>
+          <td class="text-center">
+            
+          </td>
+          <td class="text-center">
+            
+          </td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+
+</div>
+@endsection
+
+@section('js')
+/////////////////////////////////////////////////////////////////////////////////////////
+// datatables
+$.fn.dataTable.moment( 'D MMM YYYY' );
+$.fn.dataTable.moment( 'h:mm a' );
+$('#customer').DataTable({
+	"paging": true,
+	"lengthMenu": [ [25,50,100,-1], [25,50,100,"All"] ],
+	"order": [ 2, 'desc' ],
+	responsive: true
+});
+
+
+$(function () {
+	$('[data-toggle="tooltip"]').tooltip()
+});
+@endsection
+
+@section('nonjquery')
+/////////////////////////////////////////////////////////////////////////////////////////
+@endsection
