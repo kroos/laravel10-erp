9d064a71d322c8c2b8b812db5ebd644473697f33
ipma <it.ipmaindustry@gmail.com>
2024-02-19 17:16:34 +0800

    -- appraisal -- app/Http/Controllers/HumanResources/HRDept/AppraisalListController.php -- app/Models/Staff.php -- resources/views/humanresources/hrdept/appraisal/apoint/index.blade.php -- resources/views/humanresources/hrdept/appraisal/list/index.blade.php -- resources/views/humanresources/hrdept/appraisal/mark/create.blade.php -- resources/views/humanresources/hrdept/appraisal/mark/index.blade.php

--- resources/views/humanresources/hrdept/appraisal/mark/create.blade.php
+++ resources/views/humanresources/hrdept/appraisal/mark/create.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
+
 @section('content')
 <style>
   .img1 {
--- resources/views/humanresources/hrdept/appraisal/mark/index.blade.php
+++ resources/views/humanresources/hrdept/appraisal/mark/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
+
 @section('content')
 <?php
 $user = \Auth::user()->belongstostaff->id;
