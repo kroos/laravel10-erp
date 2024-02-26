61e2aa4fcb1c0e5119f59f4d5ced57139c2614b2
kroos <dhiauddin@gmail.com>
2024-02-26 18:01:21 +0800

    -- learn livewire

--- resources/views/livewire/humanresources/hrdept/cicategoryedit.blade.php
+++ resources/views/livewire/humanresources/hrdept/cicategoryedit.blade.php
@@ -6,14 +6,17 @@
 		</div>
 	@endif
 
-	<form wire:submit></form>
-	<div class="row m-2">
-		<label for="cat" class="form-label col-sm-12">Category : </label>
-		<div class="col-sm-6">
-			<input type="text" class="form-control is-invalid" id="cat" aria-describedby="in1" value="" wire:model="category">
-			<div id="in1" class="invalid-feedback">{{ $error->message }}</div>
+	<form wire:submit.prevent="update">
+		<div class="row m-2 @error('category') is-invalid @endif">
+			<label for="cat" class="form-label col-sm-4">Category : </label>
+			<div class="col-sm-auto">
+				<input type="text" class="form-control @error('category') is-invalid @endif" id="cat" aria-describedby="in1" value="" wire:model="category">
+				@error('category') <div id="in1" class="invalid-feedback">{{ $message }}</div> @endif
+			</div>
 		</div>
-	</div>
 
-
+		<div class="offset-sm-4 col-sm-auto">
+			<button type="submit" class="btn btn-sm btn-outline-secondary m-2">Submit</button>
+		</div>
+	</form>
 </div>
