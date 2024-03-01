87ca6b8e94b24bde817a4b9ca4d6a429b26429c2
ipma <it.ipmaindustry@gmail.com>
2024-03-01 17:56:41 +0800

    -- customer -- app/Http/Controllers/Sales/SalesCustomerController.php -- composer.lock -- resources/views/sales/salescustomer/edit.blade.php -- resources/views/sales/salescustomer/index.blade.php -- resources/views/sales/salescustomer/show.blade.php

---
+++ resources/views/sales/salescustomer/edit.blade.php
@@ -0,0 +1,111 @@
+@extends('layouts.app')
+
+@section('content')
+<style>
+  /* div,
+  table,
+  tr,
+  td {
+    border: 1px solid black;
+  } */
+</style>
+
+<?php
+$no = 1;
+?>
+
+<div class="container">
+  @include('sales.salesdept.navhr')
+
+  <div class="row mt-3">
+    <div class="col-md-2">
+      <h4>Customer Details</h4>
+    </div>
+  </div>
+
+  {{ Form::model($customer, ['route' => ['salescustomer.update', $customer->id], 'method' => 'PATCH', 'id' => 'form', 'class' => 'form-horizontal', 'autocomplete' => 'off', 'files' => true]) }}
+
+  <div class="row mt-4">
+    <div class="col-md-2" style="background-color: #e6e6e6">
+      Customer
+    </div>
+    <div class="col-md-10">
+      {{ Form::text('customer', @$value, ['class' => 'form-control form-control-sm', 'id' => 'customer', 'placeholder' => 'Customer', 'autocomplete' => 'off']) }}
+    </div>
+  </div>
+
+  <div class="row mt-3">
+    <div class="col-md-2" style="background-color: #e6e6e6">
+      Contact
+    </div>
+    <div class="col-md-10">
+      {{ Form::text('contact', @$value, ['class' => 'form-control form-control-sm', 'id' => 'contact', 'placeholder' => 'Contact', 'autocomplete' => 'off']) }}
+    </div>
+  </div>
+
+  <div class="row mt-3">
+    <div class="col-md-2" style="background-color: #e6e6e6">
+      Phone
+    </div>
+    <div class="col-md-10">
+      {{ Form::text('phone', @$value, ['class' => 'form-control form-control-sm', 'id' => 'phone', 'placeholder' => 'Phone', 'autocomplete' => 'off']) }}
+    </div>
+  </div>
+
+  <div class="row mt-3">
+    <div class="col-md-2" style="background-color: #e6e6e6">
+      Fax
+    </div>
+    <div class="col-md-10">
+      {{ Form::text('fax', @$value, ['class' => 'form-control form-control-sm', 'id' => 'fax', 'placeholder' => 'Fax', 'autocomplete' => 'off']) }}
+    </div>
+  </div>
+
+  <div class="row mt-3">
+    <div class="col-md-2" style="background-color: #e6e6e6">
+      Area
+    </div>
+    <div class="col-md-10">
+      {{ Form::text('area', @$value, ['class' => 'form-control form-control-sm', 'id' => 'area', 'placeholder' => 'Area', 'autocomplete' => 'off']) }}
+    </div>
+  </div>
+
+  <div class="row mt-3">
+    <div class="col-md-2" style="background-color: #e6e6e6">
+      Address
+    </div>
+    <div class="col-md-10">
+      {{ Form::textarea('address', @$value, ['class' => 'form-control form-control-sm', 'id' => 'address', 'placeholder' => 'Address', 'rows' => '3']) }}
+    </div>
+  </div>
+
+  <div class="row mt-3">
+    <div class="col-md-12 text-center">
+      {!! Form::submit('Update', ['class' => 'btn btn-sm btn-outline-secondary']) !!}
+    </div>
+  </div>
+
+  {{ Form::close() }}
+
+  <div class="row mt-3">
+    <div class="col-md-12 text-center">
+      <a href="">
+        <button onclick="goBack()" class="btn btn-sm btn-outline-secondary" id="back">
+          Back
+        </button>
+      </a>
+    </div>
+  </div>
+
+</div>
+
+@endsection
+
+@section('js')
+@endsection
+
+@section('nonjquery')
+function goBack() {
+  window.history.back();
+}
+@endsection
